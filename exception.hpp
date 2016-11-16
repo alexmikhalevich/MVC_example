f6aa958a86867ea32c707b121dad5ef9ea465a1d
maked0n <alex.mikhalevich@gmail.com>
2016-11-16 07:15:33 +0300

    Exceptions

---
+++ exception.hpp
@@ -0,0 +1,32 @@
+#ifndef EXCEPTION_HPP
+#define EXCEPTION_HPP
+
+#include <string>
+
+class IException {
+	public:
+		virtual std::string what() const = 0;
+};
+
+class ExLimitExceeded : public IException {
+	public:
+		virtual std::string what() const {
+			return "This user has too many books.";
+		}
+};
+
+class ExNoBook : public IException {
+	public:
+		virtual std::string what() const {
+			return "This user doesn't have such book.";		
+		}
+};
+
+class ExUnknownCommand : public IException {
+	public:
+		virtual std::string what() const {
+			return "No such command.";
+		}
+};
+
+#endif //EXCEPTION_HPP
