70af8570cc1a7650e57b491e1aecebab45668a73
maked0n <alex.mikhalevich@gmail.com>
2016-11-28 03:06:07 +0300

    Some small fixes

--- cdatasource.hpp
+++ cdatasource.hpp
@@ -5,21 +5,36 @@
 
 class IDataSource {
 	public:
-		virtual void load_data(std::vector<std::string>>& output) = 0;
+		virtual void load_data(std::vector<std::string>& output) = 0;
 };
 
 class CDataSourceFirst : public IDataSource {
 	public:
-		virtual void load_data(std::vector<std::string>>& output) {}
-}
+		virtual void load_data(std::vector<std::string>& output) {
+			output.reserve(3);
+			output.push_back("first_book_first_source");
+			output.push_back("second_book_first_source");
+			output.push_back("third_book_first_source");
+		}
+};
 
 class CDataSourceSecond : public IDataSource {
 	public:
-		virtual void load_data(std::vector<std::string>>& output) {}
-}
+		virtual void load_data(std::vector<std::string>& output) {
+			output.reserve(3);
+			output.push_back("first_book_second_source");
+			output.push_back("second_book_second_source");
+			output.push_back("third_book_second_source");
+		}
+};
 
 class CDataSourceThird : public IDataSource {
 	public:
-		virtual void load_data(std::vector<std::string>>& output) {}
-}
+		virtual void load_data(std::vector<std::string>& output) {
+			output.reserve(3);
+			output.push_back("first_book_third_source");
+			output.push_back("second_book_third_source");
+			output.push_back("third_book_third_source");
+		}
+};
 #endif //CDATASOURCE_HPP
